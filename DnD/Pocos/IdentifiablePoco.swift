7040bed07800efc961004f20a190f73f4a46d54a
jamesonweber <jameson_weber@hotmail.com>
2018-09-15 11:03:41 -0600

    Initial folder layout for views and pocos - Set up poco structure

---
+++ DnD/Pocos/IdentifiablePoco.swift
@@ -0,0 +1,30 @@
+//
+//  IdentifiablePoco.swift
+//  DnD
+//
+//  Created by Jameson Weber on 2018-09-14.
+//  Copyright © 2018 Jameson Weber. All rights reserved.
+//
+
+import Foundation
+
+/// The identifier class all pocos should inherit from
+class IdentifiablePoco {
+    
+    // -------------------------------------
+    // Properties
+    // -------------------------------------
+    
+    /// Unique idetifier for this poco
+    let guid: String?;
+    
+    // -------------------------------------
+    // Constructor and functions
+    // -------------------------------------
+    
+    /// Constructor
+    init(guid: String? = nil) {
+        self.guid = guid;
+    }
+    
+}
---
+++ DnD/Pocos/User.swift
@@ -0,0 +1,45 @@
+//
+//  User.swift
+//  DnD
+//
+//  Created by Jameson Weber on 2018-09-14.
+//  Copyright © 2018 Jameson Weber. All rights reserved.
+//
+
+import Foundation
+
+/// A poco representing a user
+class User : IdentifiablePoco {
+    
+    // -------------------------------------
+    // Properties
+    // -------------------------------------
+    
+    /// The user's email
+    let email: String?;
+    
+    /// Flag indicating if the user has a paid subscription
+    let isPremium: Bool;
+    
+    /// The user's password
+    let password: String?;
+    
+    
+    // -------------------------------------
+    // Constructor and functions
+    // -------------------------------------
+    
+    /// Constructor
+    init(
+        email: String? = nil,
+        guid: String? = nil,
+        isPremium: Bool? = nil,
+        password: String? = nil) {
+        
+        self.email = email;
+        self.isPremium = isPremium ?? false;
+        self.password = password;
+        
+        super.init(guid: guid);
+    }
+}
